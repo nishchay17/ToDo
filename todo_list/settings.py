f56211f16cc1e29c30a2e123a8cabada435fd4bf
nishchay17 <nishchay13971@gmail.com>
2020-03-22 20:07:38 +0530

    bug fix

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -132,5 +132,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
